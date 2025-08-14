b9d2814a502f0ad43e6ad010f974ecd7e8573455
LittleLudroth <joaquin.milk@protonmail.com>
2025-08-13 20:40:17 -0500

    Added additional file structure, created python file for creating json database from csv

---
+++ Database/Database_Creator/Database_Creator.py
@@ -0,0 +1,8 @@
+"""
+This script can be used to create a json file with an entry for each card in Fronts.
+It pulls the card info from the various csv files in spreadsheets.
+In order to get the necessary data, download each sheet from the mark 4 card spreadsheet 
+as a csv file, then put those csv files into the spreadsheet folder. This script assumes
+that the default name is used for each csv file.
+
+"""
