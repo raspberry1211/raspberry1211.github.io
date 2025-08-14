6334a08152842c2219d45a6a3af97928996d21b9
LittleLudroth <joaquin.milk@protonmail.com>
2025-08-13 20:51:43 -0500

    Replaced image files with the complete set, not the WIP set.

--- Database/Database_Creator/Database_Creator.py
+++ Database/Database_Creator/Database_Creator.py
@@ -4,5 +4,4 @@ It pulls the card info from the various csv files in spreadsheets.
 In order to get the necessary data, download each sheet from the mark 4 card spreadsheet 
 as a csv file, then put those csv files into the spreadsheet folder. This script assumes
 that the default name is used for each csv file.
-
 """
